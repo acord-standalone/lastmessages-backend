a8750a0de6bb2a81b3418457037fa2c4f1695842
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2023-03-17 17:29:30 +0300

    Update exchangeToken.js

--- src/utils/exchangeToken.js
+++ src/utils/exchangeToken.js
@@ -1,7 +1,17 @@
+const cache = new Map();
+
 module.exports = async function exchangeToken(token) {
   try {
-    return (await (await fetch("https://api.acord.app/auth/exchange?acordToken=" + token)).json())?.data?.id;
+    let id = (await (await fetch("https://api.acord.app/auth/exchange?acordToken=" + token)).json())?.data?.id;
+    cache.set(token, { at: Date.now(), id });
+    return id;
   } catch {
     return undefined;
   }
 }
+
+setInterval(() => {
+  cache.forEach((i, token) => {
+    if ((Date.now() - i.at) > 60000 * 5) cache.delete(token);
+  });
+}, 60000 * 5);
